f1338342c30cee7fc3673e4cb9498f980c5fd824
Omar Duarte <omarduarte@gmail.com>
2015-02-23 20:12:33 -0800

    Add Camera service

---
+++ client/www/app/core/media/media.camera.service.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.media')
+    .factory('Camera', Camera);
+
+  /* @ngInject */
+  function Camera($q) {
+    
+    var service = {
+      takePhoto: takePhoto
+    };
+
+    return service;
+
+    function takePhoto() {
+      var q = $q.defer();
+      var camera = navigator.camera;
+      var cameraOptions = {
+        destinationType : camera.DestinationType.DATA_URL,
+        sourceType : camera.PictureSourceType.CAMERA,
+        encodingType: camera.EncodingType.JPEG,
+        allowEdit : true,
+        quality: 40,
+        targetWidth: 640,
+        targetHeight: 640,
+        saveToPhotoAlbum: false
+      };
+
+      camera.getPicture(function(result) {
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, cameraOptions);
+
+      return q.promise;
+    }
+  }
+
+})();
